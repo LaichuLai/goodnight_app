718c3f7f65e8c6f4a6ad2da13ffa7fd822dff7fd
Gary Lai <cozzichilli@gmail.com>
2022-08-23 16:00:14 +0800

    chore: refactor routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json} do
     namespace :v1 do
-      get "/users", to: "users#index", as: :users
-      get "/users/:name", to: "users#show", as: :user
+      resources :users, only: [:index, :show] do
+        member do
+          resources :friends
+        end
+      end
 
       post "/sleep_records", to: "sleep_records#create", as: :create_sleep_records
       patch "/sleep_records", to: "sleep_records#update", as: :update_sleep_record
